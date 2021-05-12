d49d14c474a07c6838fac9c1a65597fcd458de16
Kabalisa <ikabalisa20@gmail.com>
2021-05-12 22:23:55 +0200

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>landing page</h1>
+      </main>
+    </div>
+  );
+};
+
+export default LandingPage;
