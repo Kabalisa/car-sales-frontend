06dd324979cd7db897e4e15bd1e994ee49c77dab
Kabalisa <ikabalisa20@gmail.com>
2021-06-03 14:20:47 +0200

    add more to slider and add advertise component

--- components/carDetails/index.jsx
+++ components/carDetails/index.jsx
@@ -1,7 +1,11 @@
-import { CarImagesSlider } from "../common";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faShareAlt } from "@fortawesome/free-solid-svg-icons";
+import { CarImagesSlider, Advertise } from "../common";
 import styles from "./styles/carDetails.module.scss";
 
 const CarDetailsComponent = () => {
+  const [activeIndex, setActiveindex] = useState(1);
   const images = [
     "/images/car1.jpeg",
     "/images/car2.jpeg",
@@ -11,10 +15,25 @@ const CarDetailsComponent = () => {
   ];
 
   return (
-    <main className="md:container">
+    <main className={styles.carDetails}>
       <section className={styles.carImageSlider}>
-        <CarImagesSlider images={images} />
+        <CarImagesSlider images={images} setActiveindex={setActiveindex} />
+        <div
+          className={styles.slidesNumber}
+        >{`${activeIndex} of ${images.length} images`}</div>
+        <div className={styles.iconsContainer}>
+          <div className={styles.iconHolder}>
+            <FontAwesomeIcon icon={faHeart} className={styles.icon} />
+          </div>
+          <div className={styles.iconHolder}>
+            <FontAwesomeIcon icon={faShareAlt} className={styles.icon} />
+          </div>
+        </div>
       </section>
+      <section className={styles.carDetails__upperPart}>
+        <Advertise />
+      </section>
+      <section className={styles.carDetails__detailsList}>detailsList</section>
     </main>
   );
 };
