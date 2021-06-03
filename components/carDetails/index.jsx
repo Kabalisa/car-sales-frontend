44be8d78e25813cc9b2a0eca320ba8fd4efd7aaf
Kabalisa <ikabalisa20@gmail.com>
2021-06-03 18:31:42 +0200

    sharemodal component

--- components/carDetails/index.jsx
+++ components/carDetails/index.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faShareAlt } from "@fortawesome/free-solid-svg-icons";
 import { CarImagesSlider, Advertise } from "../common";
+import { ShareModal } from "./shareModal";
 import styles from "./styles/carDetails.module.scss";
 
 const CarDetailsComponent = () => {
   const [activeIndex, setActiveindex] = useState(1);
+  const [ismodalOpen, setIsmodalOpen] = useState(false);
+
   const images = [
     "/images/car1.jpeg",
     "/images/car2.jpeg",
@@ -13,6 +16,10 @@ const CarDetailsComponent = () => {
     "/images/car4.jpeg",
     "/images/car5.jpeg",
   ];
+
+  const handleToggleModlal = () => {
+    setIsmodalOpen(!ismodalOpen);
+  };
 
   return (
     <main className={styles.carDetails}>
@@ -25,9 +32,13 @@ const CarDetailsComponent = () => {
           <div className={styles.iconHolder}>
             <FontAwesomeIcon icon={faHeart} className={styles.icon} />
           </div>
-          <div className={styles.iconHolder}>
+          <div
+            className={styles.iconHolder}
+            onClick={() => handleToggleModlal()}
+          >
             <FontAwesomeIcon icon={faShareAlt} className={styles.icon} />
           </div>
+          <ShareModal ismodalOpen={ismodalOpen} handleToggleModlal={handleToggleModlal} />
         </div>
       </section>
       <section className={styles.carDetails__upperPart}>
---
+++ components/carDetails/shareModal/index.jsx
@@ -0,0 +1,66 @@
+import Modal from "react-modal";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import styles from "./styles/shareModal.module.scss";
+
+const ShareModal = ({ ismodalOpen, handleToggleModlal }) => {
+  return (
+    <Modal
+      isOpen={ismodalOpen}
+      overlayClassName={styles.overlay}
+      className={styles.content}
+      onRequestClose={() => handleToggleModlal()}
+    >
+      <section className="flex flex-col">
+        <div className="flex justify-center relative">
+          <h1 className={styles.shareTile}>Share car</h1>
+          <FontAwesomeIcon
+            icon={faTimes}
+            className={styles.timesIcon}
+            onClick={() => handleToggleModlal()}
+          />
+        </div>
+        <div className="flex justify-center mt-5">
+          <input
+            readOnly
+            className={styles.sharedUrl}
+            value="https://tnydu.biz/DOywUh"
+          />
+          <button className="red-button p-3">Copy</button>
+        </div>
+        <div className="flex justify-center mt-7">
+          <Link href="www.facebook.com">
+            <a className={styles.socialLink}>
+              <img
+                alt="share with facebook"
+                className={styles.socialIcon}
+                src="/images/facebook.svg"
+              />
+            </a>
+          </Link>
+          <Link href="www.facebook.com">
+            <a className={styles.socialLink}>
+              <img
+                alt="share with twitter"
+                className={styles.socialIcon}
+                src="/images/twitter.svg"
+              />
+            </a>
+          </Link>
+          <Link href="www.facebook.com">
+            <a className={styles.socialLink}>
+              <img
+                alt="share with email"
+                className={styles.socialIcon}
+                src="/images/email.svg"
+              />
+            </a>
+          </Link>
+        </div>
+      </section>
+    </Modal>
+  );
+};
+
+export { ShareModal };
